1dd78977c9d12dac9a4ed9ed4170479535c1546a
avatar <avatarsinh.v@sgit.in>
2017-04-27 15:59:34 +0530

    Bundle  and Layout Changes Committed

---
+++ CordobaServices/Interfaces/ICommonServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CordobaServices.Interfaces
+{
+    public interface ICommonServices
+    {
+        string GetLayoutName(string HostName);
+    }
+}
--- CordobaWeb/App_Start/BundleConfig.cs
+++ CordobaWeb/App_Start/BundleConfig.cs
@@ -8,6 +8,8 @@ namespace CordobaWeb
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            ////////////////// Common /////////////////////////////
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -19,13 +21,19 @@ namespace CordobaWeb
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                    "~/Scripts/bootstrap.js",
+            bundles.Add(new ScriptBundle("~/bundles/CommonJs").Include(
+                    "~/Scripts/bootstrap.min.js",
                     "~/Scripts/bootstrap-modalmanager.js",
                     "~/Scripts/bootstrap-modal.js",
                     "~/Scripts/bootstrap-datepicker.js",
                     "~/Scripts/jquery.bootstrap-duallistbox.js",
-                     "~/Scripts/bootbox.js"));
+                     "~/Scripts/bootbox.js",
+                      "~/Scripts/bootstrap-modalmanager.js",
+                       "~/Scripts/bootstrap-modal.js",
+                       "~/Scripts/bootstrap-datepicker.js",
+                       "~/Scripts/jquery.bootstrap-duallistbox.js",
+                       "~/Scripts/bootbox.js"
+                     ));
                    
             bundles.Add(new StyleBundle("~/bundles/css").Include(
                       "~/Content/css/bootstrap.css",
@@ -40,24 +48,51 @@ namespace CordobaWeb
                       "~/Content/css/nav.css",                                              
                       "~/Content/css/jquery-ui.min.css"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                     "~/Scripts/angular.js",
+                     "~/Scripts/angular-animate.min.js",
+                     "~/Scripts/angular-dragdrop.min.js",
+                     "~/Scripts/angular-ui-router.min.js",
+                     "~/Scripts/angular-local-storage.js",
+                     "~/Scripts/angular-sanitize.js",
+                     "~/Scripts/angular-datatables.min.js"
+                     ));
 
+   
+            var appConfigBundle = new Bundle("~/bundles/appConfig");
+            appConfigBundle.Include("~/Scripts/jquery.dataTables.min.js");
+            appConfigBundle.Include("~/Scripts/dataTables.tableTools.js");
+            appConfigBundle.Include("~/Scripts/jquery.dataTables.rowReordering.js");
+            appConfigBundle.Include("~/Scripts/ng-file-upload.min.js");
+            appConfigBundle.Include("~/Scripts/ng-file-upload-shim.min.js");
+            appConfigBundle.Include("~/Scripts/textAngular-rangy.min.js");
+            appConfigBundle.Include("~/Scripts/textAngular-sanitize.js");
+            appConfigBundle.Include("~/Scripts/textAngular.min.js");
+            appConfigBundle.Include("~/Scripts/jquery.responsiveTabs.js");
+            appConfigBundle.Include("~/Scripts/menuscript.js");
+            appConfigBundle.Include("~/Scripts/toastr.js");
+            appConfigBundle.Include("~/Scripts/Chart.min.js");
+            appConfigBundle.Include("~/JS/appConfiguration.js");
+            appConfigBundle.Include("~/JS/Common.js");
+            appConfigBundle.Include("~/Scripts/bsDuallistbox.js");
+            appConfigBundle.IncludeDirectory("~/JS/Directives", "*.js", false);
+            appConfigBundle.IncludeDirectory("~/JS/Factory", "*.js", false);
+            appConfigBundle.IncludeDirectory("~/JS/Filters", "*.js", false);
+            bundles.Add(appConfigBundle);
+
+
+
+            //////////////////////////////////////////////////////////////////
             // this for Admin Theme
             bundles.Add(new StyleBundle("~/bundles/AdminCSS").Include(
-                     "~/Content/admin/css/icons/icomoon/styles.css",
-                     "~/Content/admin/css/bootstrap.css",
-                     "~/Content/admin/css/font-awesome.min.css",                    
+                     "~/Content/admin/css/icons/icomoon/styles.css",                                    
                      "~/Content/admin/css/core.css",
                      "~/Content/admin/css/components.css",
                      "~/Content/admin/css/colors.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/AdminJs").Include(
                       "~/Scripts/admin/js/plugins/loaders/pace.min.js",                      
-                      "~/Scripts/admin/js/core/libraries/bootstrap.min.js",
-                       "~/Scripts/bootstrap-modalmanager.js",
-                       "~/Scripts/bootstrap-modal.js",
-                       "~/Scripts/bootstrap-datepicker.js",
-                       "~/Scripts/jquery.bootstrap-duallistbox.js",
-                       "~/Scripts/bootbox.js",
+                      "~/Scripts/admin/js/core/libraries/bootstrap.min.js",                      
                         "~/Scripts/admin/js/plugins/loaders/blockui.min.js",
                         "~/Scripts/admin/js/plugins/visualization/d3/d3.min.js",
                        "~/Scripts/admin/js/plugins/visualization/d3/d3_tooltip.js",
@@ -70,50 +105,22 @@ namespace CordobaWeb
                        "~/Scripts/admin/js/pages/dashboard.js"                       
                       ));
 
+            var AdminControllerBundle = new Bundle("~/bundles/AdminControllers");
+            AdminControllerBundle.IncludeDirectory("~/JS/Home", "*.js", false);
+            AdminControllerBundle.IncludeDirectory("~/JS/Test", "*.js", false);
+            AdminControllerBundle.IncludeDirectory("~/JS/Admin/Country/Controllers", "*.js", false);
+            bundles.Add(AdminControllerBundle);  
       
-            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-                      "~/Scripts/angular.js",
-                      "~/Scripts/angular-animate.min.js",
-                      "~/Scripts/angular-dragdrop.min.js",
-                      "~/Scripts/angular-ui-router.min.js",
-                      "~/Scripts/angular-local-storage.js",
-                      "~/Scripts/angular-sanitize.js",
-                      "~/Scripts/angular-datatables.min.js"
-                      ));
-     
+           /////////////////////////
 
-            var appConfigBundle = new Bundle("~/bundles/appConfig");                
-            appConfigBundle.Include("~/Scripts/jquery.dataTables.min.js");
-            appConfigBundle.Include("~/Scripts/dataTables.tableTools.js");  
-            appConfigBundle.Include("~/Scripts/jquery.dataTables.rowReordering.js");
-            appConfigBundle.Include("~/Scripts/ng-file-upload.min.js");
-            appConfigBundle.Include("~/Scripts/ng-file-upload-shim.min.js");
-            appConfigBundle.Include("~/Scripts/textAngular-rangy.min.js");
-            appConfigBundle.Include("~/Scripts/textAngular-sanitize.js");
-            appConfigBundle.Include("~/Scripts/textAngular.min.js");        
-            appConfigBundle.Include("~/Scripts/jquery.responsiveTabs.js");
-            appConfigBundle.Include("~/Scripts/menuscript.js");
-            appConfigBundle.Include("~/Scripts/toastr.js");
-            appConfigBundle.Include("~/Scripts/Chart.min.js");
-            appConfigBundle.Include("~/JS/appConfiguration.js");
-            appConfigBundle.Include("~/JS/Common.js");
-            appConfigBundle.Include("~/Scripts/bsDuallistbox.js");
-            appConfigBundle.IncludeDirectory("~/JS/Directives", "*.js", false);
-            appConfigBundle.IncludeDirectory("~/JS/Factory", "*.js", false);
-            appConfigBundle.IncludeDirectory("~/JS/Filters", "*.js", false);
-            bundles.Add(appConfigBundle);
-          
+         
 
             var controllerBundle = new Bundle("~/bundles/Controllers");
             controllerBundle.IncludeDirectory("~/JS/Home", "*.js", false);
             controllerBundle.IncludeDirectory("~/JS/Test", "*.js", false);
             bundles.Add(controllerBundle);
 
-            var AdminControllerBundle = new Bundle("~/bundles/AdminControllers");
-            AdminControllerBundle.IncludeDirectory("~/JS/Home", "*.js", false);
-            AdminControllerBundle.IncludeDirectory("~/JS/Test", "*.js", false);
-            AdminControllerBundle.IncludeDirectory("~/JS/Admin/Country/Controllers", "*.js", false);
-            bundles.Add(AdminControllerBundle);   
+            
 
         }
     }
