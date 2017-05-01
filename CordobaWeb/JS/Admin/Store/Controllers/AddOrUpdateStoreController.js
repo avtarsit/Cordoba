dd3fa28606e13c20aed6a6f996bf15c07a3160c1
avatar <avatarsinh.v@sgit.in>
2017-05-01 14:04:04 +0530

    Store

--- CordobaWeb/JS/Admin/Store/Controllers/AddOrUpdateStoreController.js
+++ CordobaWeb/JS/Admin/Store/Controllers/AddOrUpdateStoreController.js
@@ -4,6 +4,7 @@
     decodeParams($stateParams);
     BindToolTip();
     Tab();
+    $scope.StoreObj = new Object();
     $scope.IsEditMode = false;
     if ($stateParams.StoreID != undefined && $stateParams.StoreID != null) {
         $scope.PageTitle = "Update Store";
@@ -12,9 +13,119 @@
     else {
         $scope.PageTitle = "Add Store";
     }
-    //#endregion
+    //#endregion    
+    /// Get Template List-- START---
+
+    $scope.TemplateList = [
+                        { 'TemplateId': 1, 'TemplateName':'Default Theme' }
+                      , { 'TemplateId': 2, 'TemplateName':'Default'}
+                      , { 'TemplateId': 3, 'TemplateName':'Default-2'}
+                    ];
+
+    /////-----END
+    $scope.StoreObj.TemplateId = 1; // Temporary for Selected Default Theme
 
 
+    //--- Get Layout List -- START
+    $scope.LayoutList = [
+                    { 'LayoutId': 1, 'LayoutName': 'Account' }
+                  , { 'LayoutId': 2, 'LayoutName': 'Affiliate' }
+                  , { 'LayoutId': 3, 'LayoutName': 'Categories' }
+                  , { 'LayoutId': 4, 'LayoutName': 'Checkout' }
+                  , { 'LayoutId': 5, 'LayoutName': 'Compare' }
+                  , { 'LayoutId': 6, 'LayoutName': 'Contact' }
+                  , { 'LayoutId': 7, 'LayoutName': 'Default' }
+                  , { 'LayoutId': 8, 'LayoutName': 'default2 - Layout Block Demo' }
+                  , { 'LayoutId': 9, 'LayoutName': 'Home' }
+                  , { 'LayoutId': 10, 'LayoutName': 'Information' }
+                  
+    ];
+
+    /////-----END
+    $scope.StoreObj.LayoutId = 1; // Temporary for Selected Default Theme
+
+
+    //-----END
+
+
+    ///Get Country List --- START
+    $scope.GetCountryList = function () {
+        $http.get(configurationService.basePath + "api/CountryApi/GetCountryList?CountryCd=''")
+          .then(function (response) {
+              if (response.data.length > 0) {
+                  $scope.CountryList = response.data;
+              }
+          })
+      .catch(function (response) {
+
+      })
+      .finally(function () {
+
+      });
+    }
+
+    $scope.GetCountryList();
+    $scope.StoreObj.CountryCd = ' IN';
+    ///END
+ 
+    ///Get Country List --- START
+    //$scope.GetStateList = function () {
+    //    $http.get(configurationService.basePath + "api/StateApi/GetStateList?CountryCd=''")
+    //      .then(function (response) {
+    //          if (response.data.length > 0) {
+    //              $scope.StateList = response.data;
+    //          }
+    //      })
+    //  .catch(function (response) {
+
+    //  })
+    //  .finally(function () {
+
+    //  });
+    //}
+
+    $scope.StateList = [
+                { 'StateId': 1, 'StateName': 'Andaman and Nicobar Islands' }
+              , { 'StateId': 2, 'StateName': 'Andhra Pradesh' }
+              , { 'StateId': 3, 'StateName': 'Arunachal Pradesh' }
+              , { 'StateId': 4, 'StateName': 'Assam' }
+              , { 'StateId': 5, 'StateName': 'Bihar' }
+              , { 'StateId': 6, 'StateName': 'Chandigarh' }
+              , { 'StateId': 7, 'StateName': 'Dadra and Nagar Haveli' }
+              , { 'StateId': 8, 'StateName': 'Daman and Diu' }
+              , { 'StateId': 9, 'StateName': 'Delhi' }
+              , { 'StateId': 10, 'StateName': 'Goa' }
+
+    ];    
+    ///END
+
+
+    ///-- Get Language List ----START
+
+    $scope.LanguageList = [
+        { 'LanguageCd': 'en', 'LanguageName': 'English' }
+      , { 'LanguageCd': 'de', 'LanguageName': 'Deutsch' }
+      , { 'LanguageCd': 'fr', 'LanguageName': 'Français' }
+      , { 'LanguageCd': 'it', 'LanguageName': 'Italiano' }
+      , { 'LanguageCd': 'es', 'LanguageName': 'Español' }
+      , { 'LanguageCd': 'pt', 'LanguageName': 'Português' }
+      , { 'LanguageCd': 'nl', 'LanguageName': 'Nederlands' }
+    ];
+
+    $scope.StoreObj.LanguageCd='en';
+    ///END
+
+
+    ///-- Get Currency List ----START
+
+    $scope.CurrencyList = [
+        { 'CurrencyCd': 'P41', 'CurrencyName': 'Points (1=41p)' }
+      , { 'CurrencyCd': 'P50', 'CurrencyName': 'Points (1=50p)' }
+      , { 'CurrencyCd': 'GBP', 'CurrencyName': 'Pound Sterling' }
+    ];
+
+    $scope.StoreObj.CurrencyCd = 'P50';
+    ///END
 
     $scope.DeleteStore = function () {
         bootbox.dialog({
