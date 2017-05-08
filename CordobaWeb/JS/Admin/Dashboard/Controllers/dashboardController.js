a996c6c9e2edcc9dab2305709d06eda7073fa9e6
avatarsinh <avtarsinh.v@sgit.in>
2017-05-08 12:39:26 +0530

    no message

--- CordobaWeb/JS/Admin/Dashboard/Controllers/dashboardController.js
+++ CordobaWeb/JS/Admin/Dashboard/Controllers/dashboardController.js
@@ -64,6 +64,8 @@
                 var Top5Sales_Chart = ec.init(document.getElementById('Top5Sales_Chart'), limitless);
                 var Top5Customer_Chart = ec.init(document.getElementById('Top5Customer_Chart'), limitless);
 
+                var Top5Product_Chart = ec.init(document.getElementById('Top5Product_Chart'), limitless);
+                
 
                 // Charts setup
                 // ------------------------------                    
@@ -218,14 +220,14 @@
                             //    title: 'View data',
                             //    lang: ['View chart data', 'Close', 'Update']
                             //},
-                            //magicType: {
-                            //    show: true,
-                            //    title: {
-                            //        pie: 'Switch to pies',
-                            //        funnel: 'Switch to funnel',
-                            //    },
-                            //    type: ['pie', 'funnel']
-                            //},
+                            magicType: {
+                                show: true,
+                                title: {
+                                    bar: 'Switch to bar',
+                                    line: 'Switch to line',
+                                },
+                                type: ['line', 'bar']
+                            },
                             restore: {
                                 show: true,
                                 title: 'Restore'
@@ -303,7 +305,8 @@
                     xAxis: [
                         {
                             type: 'category',
-                            data: ['Annodata', 'Rewards ', 'Vitrx', 'Choice', 'Example']
+                            boundaryGap: ['25%', '24%']
+                           , data: ['Annodata', 'Rewards ', 'Vitrx', 'Choice', 'Example']
                             , rotated: true
                         }
                     ],
@@ -357,8 +360,9 @@
                     xAxis: [
                         {
                             type: 'category',
-                            data: ['Paul\nGill', 'A\n Clubb ', 'AJ\nAli', 'Adil\nAli', 'Adrian\nLamb']
+                            data: ['Paul', 'Clubb ', 'AJ', 'Adil', 'Adrian']
                             , rotated: true
+                            ,boundaryGap: ['25%', '24%']
                         }
                     ],
                     yAxis: [
@@ -379,6 +383,64 @@
 
                 // END - Top 5 Customer   =  Top5Customer_Chart
 
+                // Top 5 Customer   =  Top5Customer_Chart
+                Top5Product_Chart_option = {
+                    //title: {
+                    //    text: 'Store'
+                    //    //subtext: 'dsfg'
+                    //},
+                    tooltip: {
+                        trigger: 'axis'
+                    },
+                    //legend: {
+                    //    data: ['sdfds']
+                    //},
+                    toolbox: {
+                        show: true,
+                        orient: 'vertical',
+                        feature: {
+                            //mark: { show: true },
+                            //dataView: { show: true, readOnly: false },
+                            //magicType: { show: true, type: ['line', 'bar'], title: 'Switch to line', },
+                            magicType: {
+                                show: true,
+                                title: {
+                                    bar: 'Switch to pies',
+                                    line: 'Switch to line',
+                                },
+                                type: ['bar', 'line']
+                            },
+                            restore: { show: true, title: 'Restore' },
+                            saveAsImage: { show: true, title: 'Same as image' }
+                        }
+                    },
+                    calculable: false,
+                    xAxis: [
+                        {
+                            type: 'category',
+                            data: ['Marks £50', 'Amazon £50 Gift Code ', 'Amazon £100', 'Marks £50', 'Capital £75']
+                        }
+                    ],
+                    yAxis: [
+                        {
+                            type: 'value'
+                        }
+                    ],
+                    series: [
+                        {
+                            name: 'Product',
+                            type: 'bar',
+                            data: [2.0, 2.6, 20.0, 6.4, 3.3],
+                        }
+                    ]
+                };
+
+
+
+                // END - Top 5 Customer   =  Top5Customer_Chart
+
+
+
                 // Apply options
                 // ------------------------------
 
@@ -386,25 +448,20 @@
                 SalesAnalytics.setOption(SalesAnalytics_options);
                 Top5Sales_Chart.setOption(Top5Sales_Chart_option);
                 Top5Customer_Chart.setOption(Top5Customer_Chart_option);
-
+                Top5Product_Chart.setOption(Top5Product_Chart_option);
+                
 
                 // Resize charts
                 // ------------------------------
 
                 window.onresize = function () {
                     setTimeout(function () {
-                        //basic_pie.resize();
-                        //basic_donut.resize();
-                        //nested_pie.resize();
-                        //infographic_donut.resize();
+                     
                         OrderSummary.resize();
                         SalesAnalytics.resize();
                         Top5Sales_Chart.resize();
                         Top5Customer_Chart.resize();
-                        //rose_diagram_visible.resize();
-                        //lasagna_donut.resize();
-                        //pie_timeline.resize();
-                        //multiple_donuts.resize();
+                        Top5Product_Chart.resize();
                     }, 200);
                 }
             }
