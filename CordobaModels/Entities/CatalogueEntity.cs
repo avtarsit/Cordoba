21f0642b1d7853b2b744b954b04ed37412c743a4
avatarsinh <avtarsinh.v@sgit.in>
2017-05-12 19:01:16 +0530

    no message

--- CordobaModels/Entities/CatalogueEntity.cs
+++ CordobaModels/Entities/CatalogueEntity.cs
@@ -10,5 +10,7 @@ namespace CordobaModels.Entities
     {
         public int catalogue_Id { get; set; }
         public string Name { get; set; }
+
+        public bool? IsSelected { get; set; }
     }
 }
