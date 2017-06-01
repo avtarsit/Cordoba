44f51bf5555c1b496877dae9f52603572c9cab40
pavan <pavan.a@sgit.in>
2017-06-01 11:11:30 +0530

    Report

---
+++ CordobaAPI/API/ReportApiController.cs
@@ -0,0 +1,52 @@
+﻿using CordobaModels.Entities;
+using CordobaServices.Interfaces;
+using CordobaServices.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CordobaServices.Helpers;
+
+namespace CordobaAPI.API
+{
+    public class ReportApiController : ApiController
+    {
+        public IReportService _reportServices;
+
+        public ReportApiController()
+        {
+            _reportServices = new ReportService();
+        }
+
+        [HttpPost]
+        public TableParameter<ReportEntity> GetReturnList(int PageIndex, DateTime DateStart, DateTime DateEnd, int GroupById, int StatusId , TableParameter<ReportEntity> tableParameter)
+        {
+            try
+            {
+                tableParameter.PageIndex = PageIndex;
+                string sortColumn = tableParameter.SortColumn.Desc ? tableParameter.SortColumn.Column + " desc" : tableParameter.SortColumn.Column + " asc";
+                var result = _reportServices.GetReturnList(sortColumn, DateStart, DateEnd, GroupById, StatusId, tableParameter, "").ToList();
+
+                int totalRecords = 0;
+                if (result != null && result.Count > 0)
+                {
+                    totalRecords = result.FirstOrDefault().TotalRecords;
+                }
+
+                return new TableParameter<ReportEntity>
+                {
+                    aaData = result.ToList(),
+                    iTotalRecords = totalRecords,
+                    iTotalDisplayRecords = totalRecords
+                };
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
--- CordobaWeb/JS/Admin/Report/Controllers/ShippingReportController.js
+++ CordobaWeb/JS/Admin/Report/Controllers/ShippingReportController.js
@@ -6,11 +6,36 @@
     createDatePicker();
 
     //#endregion  
-    $scope.dtOptions = DTOptionsBuilder.newOptions()
-                     .withOption('bDestroy', true)
+    //$scope.dtOptions = DTOptionsBuilder.newOptions()
+    //                 .withOption('bDestroy', true)
 
     $scope.PageTitle = "Sales Reports - Shipping";
 
+    $scope.GroupBy = [
+       { id: 0, name: 'Years' },
+       { id: 1, name: 'Months' },
+       { id: 2, name: 'Weeks' },
+       { id: 3, name: 'Days' }
+    ];
+
+
+    $scope.OrderStatus = [
+       { id: 0, name: 'All Statuses' },
+       { id: 1, name: 'Processing' },
+       { id: 2, name: 'Shipped' },
+       { id: 3, name: 'PartiallyShipped' },
+       { id: 4, name: 'Returned' },
+       { id: 5, name: 'Cancelled' }
+    ];
+
+
+    $scope.filter = {
+        DateStart: '',
+        DateEnd: '',
+        ShippingTitle: '',
+        NoOrders: '',
+        Total: ''
+    };
 
     //$scope.GetManufacturersList = function () {
     //    $http.get(configurationService.basePath + "api/ManufacturersApi/GetManufacturersList?ManufacturersID=0")
@@ -27,7 +52,123 @@
     //  });
     //}
     //$scope.GetManufacturersList();
+    function BindSearchCriteria(aoData) {
 
+        aoData.push({ 'name': 'searchKey', 'value': '' });
+        aoData.push({ 'name': 'Operation', 'value': '' });
+        aoData.push({ 'name': 'searchValue', 'value': '' });
+
+        return aoData;
+    }
+
+
+    function BindSorting(aoData, oSettings) {
+        debugger;
+        angular.forEach(oSettings.aaSorting, function (row, i) {
+            var sortObj = new Object();
+            sortObj.Column = oSettings.aoColumns[row[0]].mData;
+            sortObj.Desc = row[1] == 'desc';
+            aoData.push({ 'name': 'SortColumns', 'value': JSON.stringify(sortObj) });
+            return;
+        });
+        return aoData;
+    }
+
+    $scope.GetOrderList = function () {
+
+        if ($.fn.DataTable.isDataTable("#tblShippingReport")) {
+            $('#tblShippingReport').DataTable().destroy();
+        }
+
+        //var table;
+        var table = $('#tblShippingReport').DataTable({
+            stateSave: false,
+            "oLanguage": {
+                "sProcessing": "",
+                "sZeroRecords": "<span class='pull-left'>No records found</span>",
+            },
+            "searching": true,
+            "dom": '<"table-responsive"rt><"bottom"lip<"clear">>',
+            "bProcessing": true,
+            "bServerSide": true,
+            "iDisplayStart": 0,
+            "iDisplayLength": configurationService.pageSize,
+            "lengthMenu": configurationService.lengthMenu,
+            "sAjaxDataProp": "aaData",
+            "aaSorting": [[0, 'desc']],
+            "sAjaxSource": configurationService.basePath + 'api/OrderApi/GetOrderList',
+            "fnServerData": function (sSource, aoData, fnCallback, oSettings) {
+
+                aoData = BindSearchCriteria(aoData);
+
+                aoData = BindSorting(aoData, oSettings);
+                var PageIndex = parseInt($('#tblShippingReport').DataTable().page.info().page) + 1;
+                oSettings.jqXHR = $.ajax({
+                    'dataSrc': 'aaData',
+                    "dataType": 'json',
+                    "type": "POST",
+                    "url": sSource + "?PageIndex=" + PageIndex,
+                    "data": aoData,
+                    "success": fnCallback,
+                    "error": function (data, statusCode) {
+                        //exceptionService.ShowException(data.responseJSON, data.status);
+                    }
+                });
+            },
+
+            "aoColumns": [
+
+                {
+                    "mData": "date_start",
+                    "bSortable": true,
+                    "type": "date",
+                    "format": 'DD-MM-YYYY',
+                },
+                {
+                    "mData": "date_end",
+                    "bSortable": true
+                },
+                //{
+                //    "mData": "order_id",
+                //    "bSortable": true,
+                //    "sClass": " text-right"
+                //    //"render": function (data, type, row) {
+                //    //    return '<a data-Id=' + row.JobId + ' class="cursor-pointer" ng-click="EditJobCode($event)">' + data + '</a>'
+                //    //}
+                //},
+                {
+                    "mData": "shippingTitle",
+                    "bSortable": true
+                },
+                {
+                    "mData": "noOfOrders",
+                    "bSortable": true
+                },
+                {
+                    "mData": "total",
+                    "bSortable": true,
+                    "sClass": "text-right"
+                },
+
+                
+                //{
+                //    "mData": null, "bSortable": false,
+                //    "sClass": "action text-right",
+                //    "render": function (data, type, row) {
+                //        return '<a ui-sref="Orders({OrderId:' + row.order_id + '})"><i class="glyphicon glyphicon-eye-close cursor-pointer" title="view"></i></a> &nbsp;  <a ui-sref="ManageOrder({orderId:' + row.order_id + '})"><i class="glyphicon glyphicon-edit cursor-pointer" title="edit"></i></a>  &nbsp;  <i class="glyphicon glyphicon-erase cursor-pointer" title="delete"></i>'
+                //    }
+                //},
+            ],
+            "fnCreatedRow": function (nRow, aData, iDataIndex) {
+                $compile(angular.element(nRow).contents())($scope);
+            },
+            "fnDrawCallback": function () {
+                BindToolTip();
+            }
+        });
+    }
+
+    $scope.GetOrderList();
 
 
 
