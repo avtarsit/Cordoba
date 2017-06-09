06be23ffa40e6fae9681292491afacd7c8db4d88
avatar <avatarsinh.v@sgit.in>
2017-06-09 10:23:41 +0530

    Css ,AddtoCart

--- CordobaWeb/JS/Layout/AddProductToCart/Controllers/AddProductToCartController.js
+++ CordobaWeb/JS/Layout/AddProductToCart/Controllers/AddProductToCartController.js
@@ -6,30 +6,17 @@
     $scope.StoreDetailInSession = StoreSessionDetail;
     //#endregion  
     $scope.AddProductToCart = function (ProductObj,e) {
-        
-        //e.preventDefault();
-        //e.stopPropagation();
-      
-        //e.target.setAttribute('disabled', 'disabled');
-        //e.target.classList.add('loading');
-        /////////////////
+            
         debugger;
-       
-
       ////////////////
-        $http.get(configurationService.basePath + "API/ProductApi/AddProductToCart?store_id=" + $scope.StoreDetailInSession.store_id + "&customer_id=" + UserDetail.customer_id + "&product_id=" + ProductObj.product_id + "&qty=1&cartgroup_id=" + UserDetail.cartgroup_id)
+        $http.get(configurationService.basePath + "API/ProductApi/AddProductToCart?store_id=" + $scope.StoreDetailInSession.store_id + "&customer_id=" + UserDetail.customer_id + "&product_id=" + ProductObj.product_id + "&qty=1&cartgroup_id=" + (UserDetail.cartgroup_id==null?0:UserDetail.cartgroup_id))
           .then(function (response) {
               CartAnimation($(e.target));
               //toastr.success("Item successfully added in cart.");
               UserDetail.cartgroup_id = response.data.cartgroup_id;
               UserDetail.TotalItemAdded = response.data.TotalItemAdded;
               $rootScope.CustomerDetail = UserDetail;
-              localStorageService.set("loggedInUser", UserDetail);
-              //setTimeout(function () {
-              //    e.target.classList.remove('loading');
-              //    e.target.removeAttribute('disabled');
-              //},0);
-          
+              localStorageService.set("loggedInUser", UserDetail);             
           })
       .catch(function (response) {
 
