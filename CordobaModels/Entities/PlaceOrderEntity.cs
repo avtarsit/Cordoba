18978ab643d7adf5fb58b0d24b192ccbddf0acd3
pavan <pavan.a@sgit.in>
2017-06-21 19:04:34 +0530

    Email for order history

--- CordobaModels/Entities/PlaceOrderEntity.cs
+++ CordobaModels/Entities/PlaceOrderEntity.cs
@@ -15,5 +15,29 @@ namespace CordobaModels.Entities
        public string Comment { get; set; }
        public int? CartGroupId { get; set; }
 
+       // Order Detail for Email
+       public int order_id { get; set; }
+       public int invoice_id { get; set; }
+       public string store_name { get; set; }
+       public string store_logo { get; set; }
+       public string customer_name { get; set; }
+       public string email { get; set; }
+       public string telephone { get; set; }
+       public string product_name { get; set; }
+       public int quantity { get; set; }
+       public decimal tax { get; set; }
+       public decimal product_price { get; set; }
+       public decimal total { get; set; }
+       public string OrderStatusName { get; set; }
+       public string payment_name { get; set; } //order status name
+       public string payment_address { get; set; }
+       public string payment_method { get; set; }
+       //public string payment_company { get; set; }
+       public string shipping_address { get; set; }
+       public string shipping_name { get; set; }
+       public string shipping_company { get; set; }
+       public string shipping_method { get; set; }
+       public string currencyTitle { get; set; }
+
     }
 }
