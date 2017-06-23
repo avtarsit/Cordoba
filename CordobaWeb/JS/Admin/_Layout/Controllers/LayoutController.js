2a9f4a382f7cee5f71280ddff86610f7d87dc04e
amitsit <amit.p@shaligraminfotech.com>
2017-06-23 17:13:22 +0530

    layout

---
+++ CordobaWeb/JS/Admin/_Layout/Controllers/LayoutController.js
@@ -0,0 +1,10 @@
+﻿app.controller('LayoutController', function DemoController(AdminUserDetail, $timeout, $state, $http, $rootScope, $stateParams, $filter, $scope, $window, $state, notificationFactory, configurationService, $compile, $interval, DTColumnDefBuilder, DTOptionsBuilder, $http, $log, $q) {
+    
+    this.AdminUserDetail = AdminUserDetail;
+    console.log(this.AdminUserDetail);
+    debugger;
+    $rootScope.storeId = this.AdminUserDetail.store_id;
+    $rootScope.loggedInUserId = this.AdminUserDetail.user_id;
+    $rootScope.userGroupId = this.AdminUserDetail.user_group_id
+    
+});
